2cc507fa9b395dcf3b87701bf3854c570f985afd
Stefan Deser <deser@in.tum.de>
2014-11-19 12:42:21 +0100

    [NRRPLT-1222] added context menu for changing object colour
    Change-Id: Idba6b16a84900683445b802a7d6e1295b19cbec7

--- app/scripts/esv/controllers/gz3d-view.js
+++ app/scripts/esv/controllers/gz3d-view.js
@@ -16,7 +16,7 @@
         .controller('Gz3dViewCtrl', ['$rootScope','$scope', 'bbpConfig', function($rootScope, $scope, bbpConfig) {
             GZ3D.assetsPath = bbpConfig.get("api.neurorobotics.gzweb.development1.assets");
             GZ3D.webSocketUrl = bbpConfig.get("api.neurorobotics.gzweb.development1.websocket");
-            $rootScope.GZ3D = GZ3D; 
+            $rootScope.GZ3D = GZ3D;
 
             $scope.incrementLightIntensity = function(ratio) {
                 var factor = 1 + ratio;
@@ -32,5 +32,47 @@
             $scope.pauseGazebo = function(paused) {
                 gui.emitter.emit('pause', paused);
             };
+
+            // stores, whether the context menu should be displayed
+            $scope.isShowContextMenu = false;
+
+            // the position of the context menu
+            $scope.contextMenuTop = 0;
+            $scope.contextMenuLeft = 0;
+
+            // the currently selected entity
+            $scope.entityToChange = null;
+
+            $scope.openContextMenu = function(event) {
+                var entity = scene.selectedEntity;
+                if (entity) {
+                  $scope.isShowContextMenu = true;
+                  $scope.contextMenuTop = event.clientY;
+                  $scope.contextMenuLeft = event.clientX;
+                  $scope.entityToChange = scene.selectedEntity;
+                  scene.selectedEntity = null;
+                }
+            };
+
+            // for convenience we pass just a string as "red" or "blue" currently, this will be replaced later on
+            $scope.setColorOnEntity = function(value) {
+                var colors = { red : 0xff0000, blue: 0x0000ff };
+                var entity = $scope.entityToChange;
+                var entityToChange = scene.getByName(entity.name + "::link::visual");
+
+                if (entityToChange && entityToChange.children[0] && colors[value]) {
+                  // currently we use .setHex(), because for some reason regRGB() does not respect the
+                  // brightness, i.e. when turning the lights down the objects do not get darker
+                  entityToChange.children[0].material.color.setHex(colors[value]);//setRGB(r,g,b);
+                }
+
+                // deactivate the context menu after a color was assigned
+                $scope.closeContextMenu();
+            };
+
+            $scope.closeContextMenu = function() {
+                $scope.isShowContextMenu = false;
+            };
+
         }]);
 }());
