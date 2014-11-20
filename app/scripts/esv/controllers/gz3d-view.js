c22f5dd34e64b7854d4153c744648ee8c5fde613
Stefan Deser <deser@in.tum.de>
2014-11-20 11:00:25 +0100

    [NRRPLT-1163] restricted context menu to screens, added hover info
    Change-Id: Ib5976df0e1ad2cd7e142d26973f4a1976e376291

--- app/scripts/esv/controllers/gz3d-view.js
+++ app/scripts/esv/controllers/gz3d-view.js
@@ -1,78 +1,115 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    /**
-     * @ngdoc function
-     * @name exdFrontendApp.controller:Gz3dViewCtrl
-     * @description
-     * # Gz3dViewCtrl
-     * Controller of the exdFrontendApp
-     */
+  /**
+   * @ngdoc function
+   * @name exdFrontendApp.controller:Gz3dViewCtrl
+   * @description
+   * # Gz3dViewCtrl
+   * Controller of the exdFrontendApp
+   */
 
-    /* global GZ3D: false */
-    /* global scene: false */
+  /* global GZ3D: false */
+  /* global scene: false */
 
-    angular.module('exdFrontendApp')
-        .controller('Gz3dViewCtrl', ['$rootScope','$scope', 'bbpConfig', function($rootScope, $scope, bbpConfig) {
-            GZ3D.assetsPath = bbpConfig.get("api.neurorobotics.gzweb.development1.assets");
-            GZ3D.webSocketUrl = bbpConfig.get("api.neurorobotics.gzweb.development1.websocket");
-            $rootScope.GZ3D = GZ3D;
+  angular.module('exdFrontendApp')
+    .controller('Gz3dViewCtrl', ['$rootScope', '$scope', 'bbpConfig', function ($rootScope, $scope, bbpConfig) {
+      GZ3D.assetsPath = bbpConfig.get("api.neurorobotics.gzweb.development1.assets");
+      GZ3D.webSocketUrl = bbpConfig.get("api.neurorobotics.gzweb.development1.websocket");
+      $rootScope.GZ3D = GZ3D;
 
-            $scope.incrementLightIntensity = function(ratio) {
-                var factor = 1 + ratio;
-                var lights = scene.scene.__lights;
-                var numberOfLights = lights.length;
-                for (var i = 0; i < numberOfLights; i++) {
-                  lights[i].intensity *= factor;
-                  var entity = scene.getByName(lights[i].name);
-                  scene.emitter.emit('entityChanged', entity);
-                }
-            };
+      $scope.incrementLightIntensity = function (ratio) {
+        var factor = 1 + ratio;
+        var lights = scene.scene.__lights;
+        var numberOfLights = lights.length;
+        for (var i = 0; i < numberOfLights; i++) {
+          lights[i].intensity *= factor;
+          var entity = scene.getByName(lights[i].name);
+          scene.emitter.emit('entityChanged', entity);
+        }
+      };
 
-            $scope.pauseGazebo = function(paused) {
-                gui.emitter.emit('pause', paused);
-            };
+      $scope.pauseGazebo = function (paused) {
+        gui.emitter.emit('pause', paused);
+      };
 
-            // stores, whether the context menu should be displayed
-            $scope.isShowContextMenu = false;
+      // stores, whether the context menu should be displayed
+      $scope.isContextMenuShown = false;
 
-            // the position of the context menu
-            $scope.contextMenuTop = 0;
-            $scope.contextMenuLeft = 0;
+      // the position of the context menu
+      $scope.contextMenuTop = 0;
+      $scope.contextMenuLeft = 0;
 
-            // the currently selected entity
-            $scope.entityToChange = null;
+      $scope.hoveredObject = "";
 
-            $scope.openContextMenu = function(event) {
-                var entity = scene.selectedEntity;
-                if (entity) {
-                  $scope.isShowContextMenu = true;
-                  $scope.contextMenuTop = event.clientY;
-                  $scope.contextMenuLeft = event.clientX;
-                  $scope.entityToChange = scene.selectedEntity;
-                  scene.selectedEntity = null;
-                }
-            };
+      function getModelUnderMouse(event) {
+        var pos = new THREE.Vector2(event.clientX, event.clientY);
+        var intersect = new THREE.Vector3();
+        var model = scene.getRayCastModel(pos, intersect);
+        return model;
+      }
 
-            // for convenience we pass just a string as "red" or "blue" currently, this will be replaced later on
-            $scope.setColorOnEntity = function(value) {
-                var colors = { red : 0xff0000, blue: 0x0000ff };
-                var entity = $scope.entityToChange;
-                var entityToChange = scene.getByName(entity.name + "::link::visual");
+      $scope.updateHoverInfo = function (event) {
+        var model = getModelUnderMouse(event);
+        if (model != null) {
+          $scope.hoveredObject = model.name;
+        } else {
+          $scope.hoveredObject = "";
+        }
+      };
 
-                if (entityToChange && entityToChange.children[0] && colors[value]) {
-                  // currently we use .setHex(), because for some reason regRGB() does not respect the
-                  // brightness, i.e. when turning the lights down the objects do not get darker
-                  entityToChange.children[0].material.color.setHex(colors[value]);//setRGB(r,g,b);
-                }
+      // for convenience we pass just a string as "red" or "blue" currently, this will be replaced later on
+      $scope.setColorOnEntity = function (value) {
+        if(!$scope.selectedEntity) {
+          console.error("Could not change screen color since there was no object selected");
+          return;
+        }
+        var colors = {red: 0xff0000, blue: 0x0000ff};
 
-                // deactivate the context menu after a color was assigned
-                $scope.closeContextMenu();
-            };
+        // the following line would change the color for a simple box
+        //var entityToChange = scene.getByName(scene.selectedEntity.name + "::link::visual");
 
-            $scope.closeContextMenu = function() {
-                $scope.isShowContextMenu = false;
-            };
+        // since we currently want restrict ourselves to screens we go with:
+        var entityToChange = scene.getByName($scope.selectedEntity.name + "::body::screen_glass");
 
-        }]);
+        if (entityToChange && entityToChange.children[0] && colors[value]) {
+          // currently we use .setHex(), because for some reason regRGB() does not respect the
+          // brightness, i.e. when turning the lights down the objects do not get darker
+          entityToChange.children[0].material.color.setHex(colors[value]);
+          entityToChange.children[0].material.ambient.setHex(colors[value]);
+          entityToChange.children[0].material.specular.setHex(colors[value]);
+        }
+
+        // deactivate the context menu after a color was assigned
+        $scope.toggleScreenChangeMenu(false);
+      };
+
+      // this menu is currently only displayed if the model name contains "screen"
+      $scope.toggleScreenChangeMenu = function (show, event) {
+        if (show) {
+          if (!$scope.isContextMenuShown) {
+            var model = getModelUnderMouse(event);
+            // scene.radialMenu.showing is a property of GZ3D that was originally used to display a radial menu, We are
+            // reusing it for our context menu. The reason is that this variables disables or enables the controls of
+            // scene in the render loop.
+            scene.radialMenu.showing = $scope.isContextMenuShown =
+              (model &&
+               model.name !== '' &&
+               model.name !== 'plane' &&
+               model.name.indexOf('screen') !== -1 &&
+               scene.modelManipulator.pickerNames.indexOf(model.name) === -1);
+
+            $scope.contextMenuTop = event.clientY;
+            $scope.contextMenuLeft = event.clientX;
+            $scope.selectedEntity = scene.selectedEntity;
+          }
+        }
+        else
+        {
+          $scope.isContextMenuShown = false;
+          scene.radialMenu.showing = false;
+        }
+      };
+
+    }]);
 }());
