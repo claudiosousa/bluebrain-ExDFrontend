4c4129aef3ee81720323b95fc856e674e19c6745
lguyot <luc.guyot@epfl.ch>
2014-11-24 12:37:41 +0100

    Lints only home made code
    Change-Id: If5180d414b557f760a94ef18dac4b790d4945181

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,7 +129,10 @@ module.exports = function(grunt) {
             all: {
                 src: [
                     'Gruntfile.js',
-                    '<%= yeoman.app %>/scripts/**/*.js'
+                    '<%= yeoman.app %>/scripts/controllers/**/*.js',
+                    '<%= yeoman.app %>/scripts/directives/**/*.js',
+                    '<%= yeoman.app %>/scripts/services/**/*.js',
+                    '<%= yeoman.app %>/scripts/*.js'
                 ]
             },
             test: {
