a80bbb8e40957410481be35348ee6cf26c8fc047
lguyot <luc.guyot@epfl.ch>
2015-03-26 11:53:36 +0100

    Fix JavaScript error raised when a Bad Gateway Error 502 occurs
    Change-Id: Iab0e0ab1ee89a364b7094f8ed65e6fd6b8cecad5

--- app/scripts/common/services/nrp-error-handlers.js
+++ app/scripts/common/services/nrp-error-handlers.js
@@ -5,6 +5,9 @@
 
   module.factory('serverError', ['hbpErrorService', 'hbpDialogFactory', function(hbpErrorService, hbpDialogFactory) {
     return function(response) {
+      if (response.status === 0) {
+        return; // no notification in case of unavailable servers
+      }
       hbpDialogFactory.error(hbpErrorService.httpError(response));
     };
   }]);
