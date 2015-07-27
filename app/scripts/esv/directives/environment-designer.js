86fe3bfccd62c5c4acc3316635ea2191dabe3694
Alessandro Ambrosano <a.ambrosano@sssup.it>
2015-07-27 17:07:48 +0200

    [NRRPLT-2267] Pausing the simulation while editing and restarting it once in view mode again. Play/pause/etc. buttons don't interfere anymore with the ED. Screen context menu is not shown while editing.
    Change-Id: I07a1659bd38913fb588a49736b56cb74e1cd6c09

--- app/scripts/esv/directives/environment-designer.js
+++ app/scripts/esv/directives/environment-designer.js
@@ -9,20 +9,82 @@
   });
 
   angular.module('exdFrontendApp')
-  .directive('environmentDesigner', ['$log', 'EDIT_MODE', 'panels', 'simulationSDFWorld',
-  function ($log, EDIT_MODE, panels, simulationSDFWorld) {
+  .directive('environmentDesigner', ['$log', 'EDIT_MODE', 'panels', 'simulationSDFWorld', 'STATE', 'simulationState',
+                                     '$stateParams', '$rootScope',
+  function ($log, EDIT_MODE, panels, simulationSDFWorld, STATE, simulationState, $stateParams, $rootScope) {
     return {
       templateUrl: 'views/esv/environment-designer.html',
       restrict: 'E',
       link: function(scope, element, attrs) {
         scope.EDIT_MODE = EDIT_MODE;
-        scope.mode = EDIT_MODE.view;
+        scope.mode = EDIT_MODE.VIEW;
+        scope.oldState = undefined;
 
-        scope.setEditMode = function (mode) {
-          $log.debug('ED: Setting mode to ' + mode + '.');
-          scope.mode = mode;
-          scope.$root.scene.setManipulationMode(mode);
-          panels.close();
+        scope.setEditMode = function(mode) {
+          if (scope.mode === mode) {
+            $log.debug("Mode " + mode + " is already set.");
+            panels.close();
+            return;
+          }
+
+          var setMode = function() {
+            $log.debug('Setting mode ' + mode + '.');
+            scope.mode = mode;
+            $rootScope.scene.setManipulationMode(mode);
+            switch (mode) {
+              case EDIT_MODE.VIEW:
+                $rootScope.isInEditMode = false;
+                $rootScope.toggleScreenChangeMenu(false);
+                break;
+              case EDIT_MODE.TRANSLATE:
+              case EDIT_MODE.ROTATE:
+                $rootScope.isInEditMode = true;
+                break;
+            }
+            panels.close();
+          };
+
+          var setNewState = function(state) {
+            simulationState(scope.serverBaseUrl).update(
+              {sim_id: $stateParams.simulationID}, {state: state},
+              function(data) {
+                if (data.state !== state) {
+                  $log.debug('Something went wrong when setting state.');
+                  panels.close();
+                  return;
+                }
+                setMode();
+              }
+            );
+          };
+
+          var onStateFetched = function(state) {
+            var newState;
+            if (scope.oldState === undefined) { scope.oldState = state; }
+            switch (mode) {
+              case EDIT_MODE.VIEW:
+                if (state === STATE.PAUSED) { newState = scope.oldState; }
+                break;
+              case EDIT_MODE.TRANSLATE:
+              case EDIT_MODE.ROTATE:
+                scope.oldState = state;
+                if (state === STATE.STARTED) { newState = STATE.PAUSED; }
+                break;
+            }
+            if (newState) {
+              $log.debug('Changing the simulation state to ' + newState + '.');
+              setNewState(newState);
+            }
+            else {
+              $log.debug('No need to change the simulation state.');
+              setMode();
+            }
+          };
+
+          simulationState(scope.serverBaseUrl).state(
+            {sim_id: $stateParams.simulationID},
+            function(data) { onStateFetched(data.state); }
+          );
         };
 
         scope.exportSDFWorld = function () {
