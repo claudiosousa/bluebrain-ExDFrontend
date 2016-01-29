8626037ab8631e40acf7b29cbd53833b46700e13
Sandro Weber <webers@in.tum.de>
2016-01-29 13:30:22 +0100

    [NRRPLT-3291] removed view/translate/rotate from ED
    Change-Id: I64f9d3ec73b1452395c8c1214574ca172c19e710

--- test/spec/directives/environment-designer.js
+++ test/spec/directives/environment-designer.js
@@ -111,24 +111,6 @@ describe('Directive: environment-designer', function () {
     expect(element.prop('outerHTML')).toContain('<!-- TEST: Environment Designer loaded correctly -->');
   });
 
-  it('should call correctly setEditMode', function () {
-    spyOn($scope, 'setEditMode');
-
-    var btns = element.find('button');
-    var viewBtn = angular.element(btns[0]),
-      translateBtn = angular.element(btns[1]),
-      rotateBtn = angular.element(btns[2]);
-
-    viewBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.VIEW);
-
-    translateBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.TRANSLATE);
-
-    rotateBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.ROTATE);
-  });
-
   it('should call correctly close panels when edit mode already set', function () {
     stateService.currentState = $scope.STATE.STARTED;
     $scope.gz3d.scene.manipulationMode = $scope.EDIT_MODE.TRANSLATE;
@@ -226,25 +208,6 @@ describe('Directive: environment-designer', function () {
     expect(simulationSDFWorld).toHaveBeenCalled();
     expect(exportSpy).toHaveBeenCalled();
   });
-
-  it('should call correctly setEditMode', function () {
-    spyOn($scope, 'setEditMode');
-
-    var btns = element.find('button');
-    var viewBtn = angular.element(btns[0]),
-      translateBtn = angular.element(btns[1]),
-      rotateBtn = angular.element(btns[2]);
-
-    viewBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.VIEW);
-
-    translateBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.TRANSLATE);
-
-    rotateBtn.triggerHandler('click');
-    expect($scope.setEditMode).toHaveBeenCalledWith($scope.EDIT_MODE.ROTATE);
-  });
-
 
   it('should call correctly addModel("box")', function () {
     spyOn($scope, 'addModel');
