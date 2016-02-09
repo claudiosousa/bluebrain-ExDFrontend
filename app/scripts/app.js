14d6a37e9c74252ea2c23592a4b10ae867bdeb88
lguyot <luc.guyot@epfl.ch>
2016-02-09 13:53:16 +0100

    [NRRPLT-3373] Display of Viz Cluster availability
    Change-Id: I97fc51b8c377377c3b2aa1cb9c8bbd1de00ec832

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,7 @@
       'objectInspectorModule',
       'pythonCodeHelperServices',
       'simulationInfoService',
+      'slurminfoService',
       'collabServices',
       'exdFrontendApp.Constants',
       'exdFrontendFilters',
