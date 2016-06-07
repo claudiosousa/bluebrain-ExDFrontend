78075cce44ae36fd2fda2a2013196f4be9f92bf0
Jacques KAISER <jkaiser@fzi.de>
2016-06-07 12:04:45 +0200

    [NRRPLT-3743] Probing viz cluster triggers single understandable error at most
    Change-Id: I8b8ff722d70bc522b2ee7395838a155dcc7556ba

--- app/scripts/esv/services/slurminfo-service.js
+++ app/scripts/esv/services/slurminfo-service.js
@@ -6,10 +6,24 @@
 
   module.factory('slurminfoService', ['$resource', 'serverError', 'bbpConfig', function ($resource, serverError, bbpConfig) {
     var baseUrl = bbpConfig.get('api.slurmmonitor.url');
+
+    // When we can't access the viz cluster frontend no error message is returned
+    // We therefore add one here
+    var errorWrapper = _.wrap(serverError.display, function(errDisplayFn, response) {
+      if (response.status === -1) {
+        response = angular.extend(response, {
+          data: "Could not probe vizualization cluster"
+        });
+      }
+      errDisplayFn(response);
+    });
+
     return $resource(baseUrl + '/api/v1/partitions/interactive', {}, {
       get: {
         method: 'GET',
-        interceptor: {responseError: serverError.display}
+        // If we can't access server frontend, only display error once
+        // Since we are probing every minute
+        interceptor: {responseError: _.once(errorWrapper)}
       }
     });
   }]);
