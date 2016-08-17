a81be47c63bd96f50f4a6ff6666be0c600e3c46b
claudio <claudio.sousa@epfl.ch>
2016-08-17 11:50:46 +0200

    [HOTFIX] not working in prod (cross-origin frame)
    Change-Id: Ic173dcf96a697013d82a05287396e13fa17a5f5c

--- app/scripts/esv/services/environment-service.js
+++ app/scripts/esv/services/environment-service.js
@@ -3,13 +3,9 @@
 
   angular.module('environmentServiceModule', []).factory('environmentService', ['$rootScope', '$location', function ($rootScope, $location) {
 
-    function topWindowIsInDevMode() {
-      return /[?&](dev$|dev&)/.test(window.top.location.href); //when in a iframe, test top window url
-    }
-
     function initialize() {
       $rootScope.$on('$locationChangeStart', function () {
-        $rootScope.devMode = !!$location.search().dev || topWindowIsInDevMode();
+        $rootScope.devMode = !!$location.search().dev;
       });
     }
     return {
