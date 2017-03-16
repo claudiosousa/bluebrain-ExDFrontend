2e816df5c69a244e55895d54b6403175e1caf7f8
claudio <claudio.sousa@epfl.ch>
2017-03-16 16:18:03 +0100

    [NRRPLT-4863] cleanup saveCSVRecordersFiles ctx-id
    Change-Id: I5ed82286f4bed792aef9f582f18c298e47b5548d

--- app/scripts/esv/directives/transfer-function-editor.js
+++ app/scripts/esv/directives/transfer-function-editor.js
@@ -374,7 +374,6 @@
         scope.saveCSVIntoCollabStorage = function () {
           scope.isSavingCSVToCollab = true;
           backendInterfaceService.saveCSVRecordersFiles(
-            simulationInfo.contextID,
             function () { // Success callback
               scope.isSavingCSVToCollab = false;
             }, function () { // Failure callback
